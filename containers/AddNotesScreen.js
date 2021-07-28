d943cc2c4ce16ffe123434d3d7d2dcf61dc1ea1d
Mayank <mayankraj051@gmail.com>
2021-07-28 07:20:03 +0530

    Added upload notes functionality

--- containers/AddNotesScreen.js
+++ containers/AddNotesScreen.js
@@ -6,12 +6,59 @@ import Tile from '../components/Tile';
 import CustomInput from '../components/CustomInput';
 import CustomButton from '../components/CustomButton';
 
+import storage from '@react-native-firebase/storage';
+import fireStore from '@react-native-firebase/firestore';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import DocumentPicker from 'react-native-document-picker';
+import RNFS from "react-native-fs";
+import { pathOr } from 'ramda';
+
 const AddNotesScreen = (props) => {
     const [topic, setTopic] = useState('');
+    const [imagePath, setImagePath] = useState("");
     const state = useSelector(state => state);
     const dispatch = useDispatch();
+    const subject = props?.route?.params?.subject;
+
+    const uploadImageToStorage = (path, name, type) => {
+        // setLoading(true);
+        console.log("Helloooo", subject, props);
+        let reference = storage().ref(`/${subject}/${topic}`);
+        let task = reference.putFile(path);
+        task
+            .then(snapshot => {
+                console.log('Image uploaded to the bucket!');
+                //getUploadedUrl(type);
+            })
+            .catch(e => {
+                console.log('uploading image error => ', e);
+                //setLoading(false);
+                //navigation.navigate('After Upload', { status: 'Failure', e })
+            });
+    }
+
     const uploadImage = () => {
-        console.log("user email", state, state?.auth?.email);
+        var options = {
+            title: 'Select Image',
+            storageOptions: {
+                skipBackup: true, // do not backup to iCloud
+                path: 'images', // store camera images under Pictures/images for android and Documents/images for iOS
+            },
+        };
+        launchImageLibrary(options, response => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker', storage());
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                let path = pathOr('', ['assets', 0, 'uri'], response);
+                console.log('Mayank::', path);
+                setImagePath(path);
+                uploadImageToStorage(path, topic, 'image');
+            }
+        });
     }
     const uploadPDF = () => {
         console.log("user email", state, state?.auth?.email);
--- containers/DisplaySubjectsScreen.js
+++ containers/DisplaySubjectsScreen.js
@@ -7,23 +7,23 @@ import Tile from '../components/Tile';
 const DisplaySubjectsScreen = (props) => {
     const state = useSelector(state => state);
     console.log("In Display Subjects --->", state);
-    const handleClick = () => {
+    const handleClick = (subject) => {
         const uploadNotes = state.uploadNotes;
         if (uploadNotes)
-            props.navigation.navigate("Add Notes");
+            props.navigation.navigate("Add Notes", { subject });
         else
-            props.navigation.navigate("Display Notes");
+            props.navigation.navigate("Display Notes", { subject });
     }
     return (
         <View style={styles.container}>
             <HeaderText />
             <ScrollView>
-                <Tile title={"Data Structures"} tileHeight={0.15} onClick={() => handleClick()} />
-                <Tile title={"Advanced Java"} tileHeight={0.15} onClick={() => handleClick()} />
-                <Tile title={"Web Development"} tileHeight={0.15} onClick={() => handleClick()} />
-                <Tile title={"Software Testing"} tileHeight={0.15} onClick={() => handleClick()} />
-                <Tile title={"MAD Labs"} tileHeight={0.15} onClick={() => handleClick()} />
-                <Tile title={"File Structures"} tileHeight={0.15} onClick={() => handleClick()} />
+                <Tile title={"Data Structures"} tileHeight={0.15} onClick={() => handleClick("Data Structures")} />
+                <Tile title={"Advanced Java"} tileHeight={0.15} onClick={() => handleClick("Advanced Java")} />
+                <Tile title={"Web Development"} tileHeight={0.15} onClick={() => handleClick("Web Development")} />
+                <Tile title={"Software Testing"} tileHeight={0.15} onClick={() => handleClick("Software Testing")} />
+                <Tile title={"MAD Labs"} tileHeight={0.15} onClick={() => handleClick("MAD Labs")} />
+                <Tile title={"File Structures"} tileHeight={0.15} onClick={() => handleClick("File Structures")} />
             </ScrollView>
         </View>
     )
